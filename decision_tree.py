9016be3bc12383bd5ba4708588c8458ef2c1e02b
berkaytrhn <berkaytrhn@gmail.com>
2021-11-19 21:46:30 +0300

    asdasd

--- decision_tree.py
+++ decision_tree.py
@@ -12,7 +12,7 @@ class DecisionTree:
     def __init__(self):
         self.head = Node()
 
-    def calculate_gains(self, _set):
+    def chose_best_feature(self, _set):
         # fetaure based entropies and information gains
 
         relative_entropy, rel_num_pos, rel_num_neg = self.calculate_entropy(_set)
@@ -28,7 +28,7 @@ class DecisionTree:
                 temp_samples = _set[temp_filter]
 
                 # calculate entropy and number of samples
-                temp_entropy, temp_pos, temp_neg = self.calculate_entropy(temp_samples)
+                temp_entropy, temp_pos, temp_neg = self.calculate_entropy(temp_samples[:,-1])
                 
                 # calculate ratio of relevant attribute
                 temp_ratio = (temp_pos+temp_neg)/(rel_num_pos+rel_num_neg)
@@ -88,97 +88,23 @@ class DecisionTree:
         return (1 if (num_pos > num_neg) else 0)
 
 
-    def ID3(self, root, examples, attributes):
-        print(f"root: ", root)
-        # NODE EKLENDİĞİNDE EKLENENİ ROOT YAP
+    def ID3(self, root, data, features):
+        if not root:
+            root = Node()
 
-        if self.labelStatusCheck(examples) == 1:
-            # tree change, all positive
-            root.is_leaf = True
-            root.leaf_value = 1
-            print("all 1 leaf ->",root)
-            return 
-        elif self.labelStatusCheck(examples) == 0: 
-            # tree change, all negative
-            root.is_leaf = True
-            root.leaf_value = 0
-            print("all 0 leaf ->",root)
-            return 
-        elif attributes.size == 0:
-            # mostCommonTargetAttribute(examples) operations
-            print(f"examples: {examples}")
-            res = self.mostCommonTargetAttribute(examples)
-
-            root.is_leaf = True
-            root.leaf_value = res
-            print("most common leaf ->",root)
-            return 
-        else:
-            a = self.calculate_gains(examples)[0]
-            
-            values = np.unique(examples[:,a])
-
-            
-            print("**********************************")
-            print(f"Selected feature: {a}")
-            print(f"Shape: {examples.shape}")
-            for key in values:
-                
-                """
-                {
-                    '0': filtered1,
-                    '1': filtered2
-                }
-                """
-                _filter = examples[:,a] == key
-                new_examples = examples[_filter]
-                
-                #temp_node.children[key] = new_examples
-                
-
-                print(f"{a} -> For {key}")#: {new_examples}")
-                res = self.labelStatusCheck(new_examples)
-                if not res == -1:
-                    leaf_node = Node()
-                    root.value = a
-                    leaf_node.is_leaf = True
-                    leaf_node.leaf_value = res
-                    root.children[key] = leaf_node
-                    print("classic leaf ->",leaf_node)
-                    return 
-                else:
-                    # remove prvious selected most gained column
-                    temp_node = Node()
-                    root.value = a
-                    root.children[key] = temp_node
-                    new_examples = np.concatenate((examples[:,:a],examples[:,a+1:]), axis=1)
-                    self.ID3(temp_node,new_examples,self.get_columns(new_examples))
-                    print("standard -> ",temp_node)
-                    #print(f"Removed -> {new_examples}")
-
-                    
-
-            
-
-            #node creation for a and assigning a to the root node
-            # each value for A (yes or no / 1 2 3 4 5), add branch to root
-            # yes/no içeren tüm subset sampleları setle (binary için sette iki liste olacak)
-            # -> setlenen şey boş ise altına branch ile leaf ekle ve mostCommonTargetAttribute çağır (setlenen ile değil bir üstüyle)
-            # -> else, ID3(root,set,attributes-A)
-
+        pass
 
     def get_columns(self, data):
         return np.array(list(range(len(data[0,:-1]))))
 
     def fit(self, X, y):
-        data = np.concatenate((X,y), axis=1)
-        #dataset_entropy, num_pos, num_neg = self.calculate_entropy(data)
-
         # setting attributes
         # features
         self.X = X
         # labels
         self.y = y
+
+        data = np.concatenate((X,y), axis=1)
 
 
         attributes = self.get_columns(data)
@@ -215,12 +141,10 @@ class DecisionTree:
         print(f"predicted: {temp.leaf_value}, target: {y}")
 
 
-    def calculate_entropy(self, X):
-        positives = X[X[:,-1]==1]
-        negatives = X[X[:,-1]==0]
+    def calculate_entropy(self, y):
 
-        num_pos = positives.shape[0]
-        num_neg = negatives.shape[0]
+        num_pos = len(y[y == 1])
+        num_neg = len(y[y == 0])
         num_total = num_pos+num_neg
         
         p_ratio = num_pos/num_total
@@ -232,6 +156,3 @@ class DecisionTree:
         entropy = (-p_ratio*log_p)-(n_ratio*log_n)
 
         return entropy, num_pos, num_neg
-
-    def calculate_gain(self, X, feature=None):
-        pass
